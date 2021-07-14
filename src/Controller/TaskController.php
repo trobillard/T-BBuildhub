2f7c7c5ccac3942a90e4d4826d89c1b71e13b816
Thibaud Robillard <thibaudrobillard@Thibauds-MacBook-Air.local>
2021-07-14 20:06:26 +0200

    pop-up login

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
-use App\Entity\Task;
-use App\Form\TaskType;
-use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+
+
 
 #[Route('/task')]
 class TaskController extends AbstractController
 {
+    // #[Route('/', name: 'task_index', methods: ['GET'])]
+    // public function index(TaskRepository $taskRepository): Response
+    // {   
+        
+    //    $tasks = $this->getProject()->getTasks();
+    //     return $this->render('task/index.html.twig', [
+    //         'tasks' => $tasks
+    //     ]);
+    // }
+
     #[Route('/', name: 'task_index', methods: ['GET'])]
     public function index(TaskRepository $taskRepository): Response
     {
@@ -20,6 +32,7 @@ class TaskController extends AbstractController
             'tasks' => $taskRepository->findAll(),
         ]);
     }
+
 
     #[Route('/new', name: 'task_new', methods: ['GET', 'POST'])]
     public function new(Request $request): Response
@@ -30,6 +43,7 @@ class TaskController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             $task->setPublished(new \DateTime());
+            $task->setProject($this->getProject());
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($task);
             $entityManager->flush();
@@ -42,6 +56,28 @@ class TaskController extends AbstractController
             'form' => $form,
         ]);
     }
+
+    // #[Route('/new', name: 'task_new', methods: ['GET', 'POST'])]
+    // public function new(Request $request): Response
+    // {
+    //     $task = new Task();
+    //     $form = $this->createForm(TaskType::class, $task);
+    //     $form->handleRequest($request);
+
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $task->setPublished(new \DateTime());
+    //         $entityManager = $this->getDoctrine()->getManager();
+    //         $entityManager->persist($task);
+    //         $entityManager->flush();
+
+    //         return $this->redirectToRoute('task_index', [], Response::HTTP_SEE_OTHER);
+    //     }
+
+    //     return $this->renderForm('task/new.html.twig', [
+    //         'task' => $task,
+    //         'form' => $form,
+    //     ]);
+    // }
 
     #[Route('/{id}', name: 'task_show', methods: ['GET'])]
     public function show(Task $task): Response
